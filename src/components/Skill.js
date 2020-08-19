1ff8897d00caf188eb5b9f1cb056a6f9b8deb897
ksrawr <kenneth.surban62@gmail.com>
2020-08-18 22:14:10 -0700

    adds initial styling for skill cards/items

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import './Skill.css';
+
 const Skills = () => {
   return(
-    <div>
-      
+    <div className="skill-container">
+      <div className="skill-subheading">
+        <div className="subheader">
+          <h1 className="text-subheader">SKILLS</h1>
+        </div>
+      </div>
+      <div className="skill-section">
+        <div className="skill-main">
+          <div className="skill-item">
+            Card 1
+          </div>
+          <div className="skill-item">
+            Card 2
+          </div>
+          <div className="skill-item">
+            Card 3
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
