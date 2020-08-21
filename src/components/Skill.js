15360c70b4e3a29d8dabf8cb23e81bf875ea2231
ksrawr <kenneth.surban62@gmail.com>
2020-08-20 22:17:06 -0700

    adds styling to skill cards

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -13,14 +13,16 @@ const Skills = () => {
       <div className="skill-section">
         <div className="skill-main">
           <div className="skill-item">
-            Card 1
+            <div className="skill-item-header">
+              <h2>JavaScript</h2>
+            </div>
           </div>
           <div className="skill-item">
             Card 2
           </div>
-          <div className="skill-item">
+          {/* <div className="skill-item">
             Card 3
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
